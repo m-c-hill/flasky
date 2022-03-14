1ecf69bb02c0af2cfe2b4b2d0963bc6cd3d29351
m-c-hill <matthewhill2595@gmail.com>
2022-03-14 15:00:34 +0000

    Add NameForm

--- hello.py
+++ hello.py
@@ -3,6 +3,10 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "y958G4V5W!Y&E"
@@ -11,8 +15,18 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 @app.route("/")
 def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
     return render_template("index.html", current_time=datetime.utcnow())
 
 
